d59f1a8297d0f091dce857f1180e920f8a7139bb
Florian GOTO <gotoflorian.pro@gmail.com>
2019-06-03 01:06:06 +0200

    vid 3.1 -end

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FinestModule } from './finest/finest.module';
 
 @Module({
-  imports: [],
+  imports: [FinestModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // create an app instance with CORS enabled
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  // helmet security-related HTTP headers
+  app.use(helmet());
+
+  // define a global validation pipe
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  await app.listen(process.env.PORT);
 }
+import { from } from 'rxjs';
 bootstrap();
